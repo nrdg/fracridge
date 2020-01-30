3199d6218385e2fab69ef70f86d9bfdaa203c177
Ariel Rokem <arokem@gmail.com>
2020-01-30 14:27:22 -0800

    Adds Matlab versions written by KNK.

---
+++ ridgeregressiongamma.m
@@ -0,0 +1,147 @@
+function [h,lambdas] = ridgeregressiongamma(X,gamma,y,tol)
+
+% function [h,lambdas] = ridgeregressiongamma(X,gamma,y,tol)
+%
+% <X> is the design matrix (n x p) with data points in the rows
+%   and regressors in the columns
+% <gamma> is a vector of one or more fractions between 0-1 (1 x f)
+% <y> is the data (n x b) with one or more outcome variables in the columns
+% <tol> (optional) is a tolerance value such that eigenvalues
+%   below the tolerance are treated as 0. Default: 1e-6.
+%
+% return:
+%  <h> as the estimated regression weights (p x f x b)
+%    for all fractional levels for all outcome variables.
+%  <lambdas> as the lambda values that correspond to the
+%    requested fractions (f x b). Note that <lambdas> can
+%    be Inf in the case that the requested fraction is 0.
+%
+% The basic idea is that we want ridge-regression solutions
+% whose vector lengths are controlled by the user. The vector
+% lengths are specified in terms of fractions of the length of
+% the full-length solution which is the ordinary least squares
+% (OLS) solution. Framing the problem in this way provides
+% several benefits: (1) we don't have to spend time figuring out
+% appropriate lambdas for each regression problem, (2) when
+% implemented appropriately, we can compute the full set of
+% solutions very efficiently and in a way that avoids the need
+% to compute X'*X (which might be large). Computational
+% benefits are likely to occur for large-scale problems
+% where p and/or b are large. For small problems, we may
+% take a hit in computational time due to various
+% overhead-related steps.
+%
+% Note that we silently ignore regressors that are all zeros.
+%
+% example:
+% y = randn(100,1);
+% X = randn(100,10);
+% h = inv(X'*X)*X'*y;
+% [h2,lambda] = ridgeregressiongamma(X,0.3,y);
+% h3 = inv(X'*X + lambda*eye(size(X,2)))*X'*y;
+% vectorlength(h)
+% vectorlength(h2)
+% vectorlength(h2) ./ vectorlength(h)
+% norm(h2-h3)
+
+% deal with inputs
+if ~exist('tol','var') || isempty(tol)
+  tol = 1e-6;
+end
+
+% internal constants
+bbig   = 10^3;    % sets huge-bias
+bsmall = 10^-3;   % sets small-bias
+bstep  = 0.25;    % step size in log10 space
+
+% ignore bad regressors (those that are all zeros)
+bad = all(X==0,1);
+if any(bad)
+  X = X(:,~bad);
+end
+
+% calc
+n = size(X,1);     % number of data points
+p = size(X,2);     % number of parameters (regressors)
+b = size(y,2);     % number of distinct outputs being modeled
+f = length(gamma); % number of fractions being requested
+
+% decompose X [this is a costly step]
+[u,s,v] = svd(X,'econ');  % when n>=p, u is n x p, s is p x p, v is p x p
+                          % when n< p  u is n x n, s is n x n, v is p x n
+
+% extract the eigenvalues
+selt = diag(s);  % p x 1 (OR n x 1)
+
+% mark eigenvalues that are essentially zero
+isbad = selt < tol;
+if any(isbad)
+  fprintf('WARNING: some eigenvalues are being treated as 0.\n');
+end
+
+% rotate the data
+ynew = u'*y;  % p x b (OR n x b)
+
+% compute the OLS solution in the rotated space
+hols_rotatedspace = bsxfun(@rdivide,ynew,selt);  % p x b (OR n x b)
+hols_rotatedspace(isbad,:) = 0;  % the solution will be 0 for eigenvalues that are essentially zero (pseudoinverse)
+
+% figure out a reasonable range for lambda at reasonable level of granularity
+val1 = bbig*selt(1)^2;              % huge bias (take the biggest eigenvalue down to ~.001 of what OLS would be)
+val2 = bsmall*min(selt(~isbad))^2;  % tiny bias (just add a small amount to the smallest eigenvalue)
+lambdagrid = [0 10.^(floor(log10(val2)):bstep:ceil(log10(val1)))];  % no bias to tiny bias to huge bias (1 x lambagrid)
+fprintf('using a lambagrid of size %d.\n',length(lambdagrid));
+
+% note that the <bstep> is a heuristic. we just need to sample lambdas at sufficient
+% granularity such that pchip interpolation will be able to find the requested
+% fractions with sufficient accuracy. the idea here is that the computations for
+% constructing solutions with different regularization amounts (see below) are cheap,
+% and so we are happy to "oversample" to some extent.
+
+% pre-compute for speed
+seltSQ = selt.^2;
+
+% pre-compute for speed
+scLG = bsxfun(@rdivide,seltSQ,bsxfun(@plus,seltSQ,lambdagrid));        % p x lambdagrid (OR n x lambdagrid)
+scLG(isbad,:) = 0;                                                     % for safety, ensure bad eigenvalues get scalings of 0
+
+% compute the ridge regression solutions and the corresponding lambdas
+h = zeros(p,f,b);
+lambdas = zeros(f,b);
+for ii=1:b
+
+  % compute vector length for each lambda
+    % OLD SLOW WAY: len = vectorlength(bsxfun(@times,scLG,hols_rotatedspace(:,ii)),1);   % 1 x lambdagrid
+  len = sqrt((scLG.^2'*hols_rotatedspace(:,ii).^2)');                     % 1 x lambdagrid
+
+  % make lengths relative to the first one (i.e. no regularization)
+  len = len / len(1);
+
+  % inspection (is the proposed interpolation scheme accurate?)
+  if 0
+    figure;
+    scatter(len,log(1+lambdagrid),'ro');
+    xlabel('length');
+    ylabel('log(1+lambdagrid)');
+  end
+
+  % sanity check that the gridding is not too coarse
+  mxgap = max(abs(diff(len)));                    % maximum gap
+  assert(mxgap < 0.2,'need to decrease bstep!');  % if this fails, bstep should be smaller
+  assert(min(len) < 0.0001);                      % if all is well, we should have sampled close to 0
+
+  % use cubic interpolation to find lambdas that achieve the desired fractional levels.
+  % we interpolate in log(1+y) space in order to help achieve nice smooth curves.
+  temp = interp1(len,log(1+lambdagrid),gamma,'pchip',NaN);             % 1 x f
+  temp = exp(temp)-1;                                                  % undo the log transform
+  temp(gamma==0) = Inf;                                                % when gamma is exactly 0, we are out of range, so handle explicitly
+  assert(all(~isnan(temp)));                                           % if all went well, no value should be NaN (but can be Inf)
+  lambdas(:,ii) = temp;
+
+  % get the solutions
+  sc = bsxfun(@rdivide,seltSQ,bsxfun(@plus,seltSQ,temp));              % p x f (OR n x f)
+  sc(isbad,:) = 0;                                                     % for safety, ensure bad eigenvalues get scalings of 0
+  temp = v*bsxfun(@times,sc,hols_rotatedspace(:,ii));                  % rotate solution to original space
+  h(:,:,ii) = temp;
+
+end
