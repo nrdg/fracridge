fc23688483988e0b38e46c2d0a2f031dcfddee33
Ariel Rokem <arokem@gmail.com>
2020-02-06 16:20:29 -0800

    Adds a notebook with some examples.

--- ridge_tools.py
+++ ridge_tools.py
@@ -4,7 +4,9 @@
 import numpy as np
 from scipy.linalg import svd
 from numpy.core.multiarray import interp
+from scipy.interpolate import interp1d
 import warnings
+
 
 # Module-wide constants
 BIG_BIAS = 10e3
@@ -12,7 +14,7 @@ SMALL_BIAS = 10e-3
 BIAS_STEP = 0.25
 
 
-def ridgeregressiongamma(X, y, fracs=None, tol=1e-6):
+def fracridge(X, y, fracs=None, tol=1e-6):
     """
     Approximates alpha parameters to match desired fractions of OLS length.
 
@@ -26,7 +28,7 @@ def ridgeregressiongamma(X, y, fracs=None, tol=1e-6):
         Data, with n number of observations and b number of simultaneous
         measurement units (e.g., channels).
 
-    fracs : float or 1d array, optional
+    fracs : 1d array, optional
         The desired fractions of the parameter vector length, relative to
         OLS solution. If 1d array, the shape is (f,). 
         Default: np.arange(.1, 1.1, .1)
@@ -34,24 +36,20 @@ def ridgeregressiongamma(X, y, fracs=None, tol=1e-6):
 
     Returns
     -------
-    coef : ndarray, shape (p, f, b)
+    coef : ndarray, shape (pp, ff, bb)
         The full estimated parameters across units of measurement for every
         desired fraction.
-    alphas : ndarray, shape (f,)
-
+    alphas : ndarray, shape (ff, bb)
+        The alpha coefficients associated with each solution
     Examples
     --------
     """
     if fracs is None: 
         fracs = np.arange(.1, 1.1, .1)
 
-    n, p = X.shape
-    b = y.shape[-1]
-    if hasattr(fracs, "__len__"):
-        fracs = np.asanyarray(fracs)
-        f = fracs.shape[0]
-    else:
-        f = 1
+    nn, pp = X.shape
+    bb = y.shape[-1]
+    ff = fracs.shape[0]
 
     # This is the expensive step:
     uu, selt, vv = svd(X, full_matrices=False)
@@ -83,18 +81,20 @@ def ridgeregressiongamma(X, y, fracs=None, tol=1e-6):
     sclg[:, isbad] = 0
 
     # Prellocate the solution
-    coef = np.empty((p, f, b))
-    alphas = np.empty((f, b))
+    coef = np.empty((pp, ff, bb))
+    alphas = np.empty((ff, bb))
     
     for vx in range(y.shape[-1]):
         newlen = (sclg @ ynew[:,vx]**2).T
         newlen = (newlen / newlen[0])
-        temp = interp(fracs, newlen[::-1], np.log(1 + alphagrid)[::-1])
-        # temp = interp1d(newlen, np.log(1 + alphagrid), bounds_error=False, fill_value="extrapolate")(fracs)
+        #temp = interp(fracs, newlen[::-1], np.log(1 + alphagrid)[::-1])
+        temp = interp1d(newlen, np.log(1 + alphagrid), bounds_error=False, fill_value="extrapolate",
+                       )(fracs)
         targetalphas = np.exp(temp) - 1
         for p in range(len(targetalphas)):
             sc = seltsq / (seltsq + targetalphas[p])
             coef[:, p, vx] = vv.T @ (sc * hols_new[:, vx])
+            
     return coef, alphas
 
 
