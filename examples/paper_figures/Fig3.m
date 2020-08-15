b818e1b9db46c54a3641a1e53fc95da9ad925faa
Kendrick Kay <kendrick@post.harvard.edu>
2020-08-15 13:15:46 -0500

    revise for gigascience submission

--- examples/paper_figures/Fig3.m
+++ examples/paper_figures/Fig3.m
@@ -1,12 +1,11 @@
-%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% WORK TO PREPARE THE DATA
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %% Simple setup
 
 % define
-stimfile = '~/nsd/nsddata_stimuli/stimuli/nsd/nsd_stimuli.hdf5';
-expfile = '~/nsd/nsddata/experiments/nsd/nsd_expdesign.mat';
-nsess = 40;                % how many of the first N NSD sessions to consider
-betaver = 'betas_fithrf';  % which beta version to load
+stimfile = 'nsd_stimuli.hdf5';
+expfile = 'nsd_expdesign.mat';
+nsess = 37;                % how many of the first N NSD sessions to consider
 subjix = 1;                % which subject [81 104 83]
 ng  = 25;                  % number of elements in contrast grid (ng x ng)
 npx = 17;                  % number of pixels that make up one grid element (npx x npx)
@@ -29,8 +28,8 @@ for p=1:length(allimix)
   im0 = h5read(stimfile,'/imgBrick',[1 1 1 allimix(p)],[3 425 425 1]);
   im0 = permute(im0,[3 2 1]);
   if p==1
-    imwrite(im0,'~/Dropbox/KKTEMP/sampleimage.png');
-    imwrite(rgb2gray(im0),'~/Dropbox/KKTEMP/sampleimagegray.png');
+    imwrite(im0,'sampleimage.png');
+    imwrite(rgb2gray(im0),'sampleimagegray.png');
   end
   im = single(rgb2gray(im0));  % convert to grayscale and to single format
   im = (im/255).^2;                            % convert to [0,1] and square to match display gamma
@@ -38,7 +37,7 @@ for p=1:length(allimix)
 end
 
 % compute a "contrast grid" from the images
-imagecon = zeros(ng,ng,length(allimix));  % 25 x 25 x 10000
+imagecon = zeros(ng,ng,length(allimix));  % 25 x 25 x images
 for rowix=1:ng
   statusdots(rowix,ng);
   for colix=1:ng
@@ -53,20 +52,20 @@ clear ims;
 
 %% Do some experimental-design preparation
 
-% when do the 3 trials for each image occur?
-trialgroup = zeros(3,length(allimix));  % 3 x images with trial indices
+% when do the trials for each image occur?
+trialgroup = cell(1,length(allimix));  % 1 x images with trial indices
 temp = exp1.subjectim(subjix,exp1.masterordering(1:750*nsess));
 for p=1:length(allimix)
-  trialgroup(:,p) = find(temp==allimix(p));
+  trialgroup{p} = find(temp==allimix(p));
 end
 
 %% Load NSD betas
 
 % load data
-data = zeros(81,104,10,750,40);  % X x Y x Z x 750 trials x sessions
+data = zeros(81,104,10,750,nsess);  % X x Y x Z x 750 trials x sessions
 for p=1:nsess
   fprintf('sess %d...',p);
-  file0 = sprintf('~/nsd/nsddata_betas/ppdata/subj%02d/func1pt8mm/%s/betas_session%02d.hdf5',subjix,betaver,p);
+  file0 = sprintf('betas_session%02d.hdf5',p);
   temp = h5read(file0,'/betas',[1 1 slicestart 1],[Inf Inf slicenum 750]);
   temp = double(temp)/300;                  % convert to double and then convert to percent signal change
   data(:,:,:,:,p) = temp;                   % record
@@ -77,23 +76,26 @@ data = calczscore(data,4);
 data(isnan(data)) = 0;  % some voxels in some sessions have invalid...
 
 % average across trials and re-z-score
-data = data(:,:,:,trialgroup);  % now: X x Y x Z x 30000
-data = blob(data,4,3)/3;        % now: X x Y x Z x 10000
+data2 = zeros([sizefull(data,3) length(trialgroup)]);  % X x Y x Z x images
+for p=1:length(trialgroup)
+  data2(:,:,:,p) = mean(data(:,:,:,trialgroup{p}),4);
+end
+data = data2; clear data2;
 data = calczscore(data,4);
 data(isnan(data)) = 0;  % some voxels in some sessions have invalid...
 
 %% Do some training/testing preparation
 
 % calculate the training/testing split
-[testix,~,trainix] = picksubset(1:10000,[5 1]);
+[testix,~,trainix] = picksubset(1:size(data,4),[5 1]);
 
 % deal with data
 ytrain = squish(data(:,:,:,trainix),3)';  % trials x voxels
 ytest  = squish(data(:,:,:,testix),3)';   % trials x voxels
 
 % prep the stim
-stim = calczscore(squish(imagecon,2),2);  % 25*25 x 10000
-imwrite(uint8(255*cmaplookup(reshapesquare(stim(:,1)),min(stim(:,1)),max(stim(:,1)),0,gray(256))),'~/Dropbox/KKTEMP/samplecon.png');
+stim = calczscore(squish(imagecon,2),2);  % 25*25 x imagees
+imwrite(uint8(255*cmaplookup(reshapesquare(stim(:,1)),min(stim(:,1)),max(stim(:,1)),0,gray(256))),'samplecon.png');
 
 % deal with stimulus
 Xtrain = stim(:,trainix)';    % trials x 625 regressors  
@@ -102,16 +104,11 @@ Xtest  = stim(:,testix)';     % trials x 625 regressors
 % clean
 clear data;
 
-%% SAVE
-
-save('/home/surly-raid1/kendrick-data/figurefiles/REALDATA.mat','ytrain','ytest','Xtrain','Xtest','allimix','testix','trainix','-v7.3','-nocompression');
-
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%% LOAD
+%% PREP
 
 % load
-load('/home/surly-raid1/kendrick-data/figurefiles/REALDATA.mat');
 ytest = ytest(:,1:81*104);     % just get first slice!
 ytrain = ytrain(:,1:81*104);   % just get first slice!
 
@@ -177,26 +174,26 @@ end
 
 %% VISUALIZE
 
-mkdirquiet('~/Dropbox/KKTEMP/figures');
+mkdirquiet('figures');
 
 % map of R2
 whsl = 1;
 [mm,ii] = max(testR2,[],2);
 for typ=1:3
   vol = reshape(mm(:,:,typ),[81 104 1]);
-  imwrite(uint8(255*makeimagestack(vol(:,:,whsl),[0 20],0)),hot(256),sprintf('~/Dropbox/KKTEMP/figures/R2_typ%d.png',typ));
-  imwrite(uint8(255*cmaplookup(makeimagestack(vol(:,:,whsl),[],0),0,20,[],hot(256))),sprintf('~/Dropbox/KKTEMP/R2_typ%d_thresh.png',typ), ...
+  imwrite(uint8(255*makeimagestack(vol(:,:,whsl),[0 20],0)),hot(256),sprintf('figures/R2_typ%d.png',typ));
+  imwrite(uint8(255*cmaplookup(makeimagestack(vol(:,:,whsl),[],0),0,20,[],hot(256))),sprintf('R2_typ%d_thresh.png',typ), ...
     'Alpha',uint8(255*(makeimagestack(vol(:,:,whsl),[],0)>20/4)))
   vol = reshape(ii(:,:,typ),[81 104 1]);
   if ismember(typ,[1 3])
     vol = 22-vol;  % flip
   end
-  imwrite(uint8(255*makeimagestack(vol(:,:,whsl),[1 21],0)),copper(256),sprintf('~/Dropbox/KKTEMP/figures/maxindex_typ%d.png',typ));
+  imwrite(uint8(255*makeimagestack(vol(:,:,whsl),[1 21],0)),copper(256),sprintf('figures/maxindex_typ%d.png',typ));
 end
 
 % T1
-a1 = load_untouch_nii('~/nsd/nsddata/ppdata/subj01/func1pt8mm/T1_to_func1pt8mm.nii.gz');
-imwrite(uint8(255*makeimagestack(double(a1.img(:,:,40)),1,0)),gray(256),sprintf('~/Dropbox/KKTEMP/figures/T1.png'));
+a1 = load_untouch_nii('T1_to_func1pt8mm.nii.gz');
+imwrite(uint8(255*makeimagestack(double(a1.img(:,:,40)),1,0)),gray(256),sprintf('figures/T1.png'));
 
 % pick a voxel
 if 0
@@ -208,7 +205,7 @@ end
 rowiis = 1:81;
 colii = 10;
 voxelix = [];
-mkdirquiet('~/Dropbox/KKTEMP/figures/rf/');
+mkdirquiet('figures/rf/');
 mxs = [];
 for zz=1:length(rowiis)
   voxelix(zz) = sub2ind([81 104 1],rowiis(zz),colii,1);
@@ -233,22 +230,17 @@ for zz=1:length(rowiis)
       finalmx = 1;
     end
     imwrite(uint8(255*makeimagestack(ok,[-finalmx finalmx+(2*finalmx)*1/255],[],[1 21])),[cmapsign4(255); 1 1 1], ...
-      sprintf('~/Dropbox/KKTEMP/figures/rf/rf_typ%d_num%02d.png',typ,zz));
+      sprintf('figures/rf/rf_typ%d_num%02d.png',typ,zz));
   end
 end
 
 figureprep([100 100 300 300]); hold on;
 imagesc((0:20)/20,[0 1]); colormap(copper(256)); colorbar;
-figurewrite('colorbarcopper',[],-1,'~/Dropbox/KKTEMP');
+figurewrite('colorbarcopper',[],-1,pwd);
 
 figureprep([100 100 300 300]); hold on;
 imagesc(1); colormap(cmapsign4(255)); colorbar;
-figurewrite('colorbarcmapsign4',[],-1,'~/Dropbox/KKTEMP');
-
-% check the R2 but not a big difference
-figure;
-scatter(mm(:,:,1),mm(:,:,2),'r.');
-axissquarify;
+figurewrite('colorbarcmapsign4',[],-1,pwd);
 
 % compute DOF for standard alphas
 [u,s,v] = svd(Xtrain,0);
@@ -373,5 +365,5 @@ for zzz=1:length(voxelix)
 
   end
 
-  figurewrite(sprintf('vx%03d',zzz),[],-1,'~/Dropbox/KKTEMP/figures/inspections');
+  figurewrite(sprintf('vx%03d',zzz),[],-1,'figures/inspections');
 end
