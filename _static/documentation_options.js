7dc83950366615b73cee2e1f772c37e047d92f23
arokem <arokem@gmail.com>
2026-01-28 05:25:46 +0000

    Deploying to gh-pages from @ nrdg/fracridge@f8b2946e237a4ecb55266da42e2abbcf067dd817 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,7 +1,6 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2.0',
-    LANGUAGE: 'None',
+const DOCUMENTATION_OPTIONS = {
+    VERSION: '3.0',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
